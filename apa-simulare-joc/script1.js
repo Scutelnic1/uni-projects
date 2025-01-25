dabd0ca883ed37ce06aaa3440e62f9e5274d1652
Scutelnic1 <scutelnicv6@gmail.com>
2025-01-25 14:44:50 +0200

    Add front
    Add front

---
+++ apa-simulare-joc/script1.js
@@ -0,0 +1,5 @@
+
+    // După 5 secunde, schimbă pagina
+    setTimeout(function() {
+        window.location.href = 'HomePage.html';  // Schimbă cu URL-ul dorit
+    }, 3000);  // 5000 ms = 5 secunde
